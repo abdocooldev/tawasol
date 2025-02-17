8d42ac995ecca0145607c6250a1db55f3c44ad8e
Abdelrahman Nasser <abdelrahman.nasseryo@gmail.com>
2025-02-17 08:44:29 +0200

    Create User Model

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      default: Date.now,
+    },
+  })
+);
+
+export default User;
